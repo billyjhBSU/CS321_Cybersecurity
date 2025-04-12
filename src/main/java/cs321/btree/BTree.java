cabcacf87c7231c570c5ce8823c7bd297d10e208
Ado Maksic <adnanmaksic@u.boisestate.edu>
2025-04-11 23:23:23 -0600

    Update

--- src/main/java/cs321/btree/BTree.java
+++ src/main/java/cs321/btree/BTree.java
@@ -1,53 +1,191 @@
 package cs321.btree;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class BTree implements BTreeInterface {
-
     private int degree;
-    private long size;
-    private long nodes;
     private BTreeNode root;
-    private long offset = 0;
+    private String filename;
+    private long nodeSize;
+    private int numNodes;
+    private ByteBuffer buffer;
+    private FileChannel fileChannel;
+    private long offset;
 
-    public BTree(int degree) throws BTreeException {
-        if (degree < 2) {
-            throw new BTreeException("Degree must be greater then or equal to 2.");
-        }
-        this.degree = degree;
-        this.nodes = 0;
-        this.size = 0;
-        this.root = null;
+    private final  int METADATA_SIZE = Long.BYTES;
+    private long nextDiskAddress = METADATA_SIZE;
+
+
+    private long rootAddress = METADATA_SIZE;
+
+    /**
+     * Creates a new {@code BTree} with default minimum degree of 2 and specified filename.
+     * @param filename the name of the file where the {@code BTree} will be stored
+     */
+    public BTree(String filename) throws BTreeException {
+        this(2, filename);
     }
 
-//    public BTree(int degree, String fileName) throws BTreeException {
-//        this(degree);
-//    }
+    /**
+     * Creates a new {@code BTree} with specified degree and filename.
+     * @param degree the minimum degree of the {@code BTree}
+     * @param filename the name of the file where the {@code BTree} will be stored
+     */
+    public BTree(int degree, String filename) throws BTreeException {
+        if (degree < 2) {
+            throw new BTreeException("Degree must be at least 2.");
+        }
 
+        this.degree = degree;
+        this.filename = filename;
+        this.nodeSize = new BTreeNode(true).BYTES; // Create dummy BTreeNode object to get non-static size
+        this.numNodes = 1; // Starts with root only
+        this.offset = -1;
+
+        // Initialize file storage
+        try {
+            File file = new File(filename);
+            if (!file.exists()) {
+                file.createNewFile();
+                RandomAccessFile raf = new RandomAccessFile(filename, "rw");
+                this.fileChannel = raf.getChannel();
+                writeMetaData();
+                root = new BTreeNode(true); //TODO need this?
+            } else {
+                RandomAccessFile raf = new RandomAccessFile(filename, "rw");
+                this.fileChannel = raf.getChannel();
+                readMetaData();
+                this.root = diskRead(rootAddress);
+            }
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
+
+    }
+
+    /**
+     * Read the metadata from the data file.
+     * @throws IOException
+     */
+    public void readMetaData() throws IOException {
+        fileChannel.position(0);
+
+        ByteBuffer tmpbuffer = ByteBuffer.allocateDirect(METADATA_SIZE);
+
+        tmpbuffer.clear();
+        fileChannel.read(tmpbuffer);
+
+        tmpbuffer.flip();
+        rootAddress = tmpbuffer.getLong();
+    }
+
+
+    /**
+     * Write the metadata to the data file.
+     * @throws IOException
+     */
+    public void writeMetaData() throws IOException {
+        fileChannel.position(0);
+
+        ByteBuffer tmpbuffer = ByteBuffer.allocateDirect(METADATA_SIZE);
+
+        tmpbuffer.clear();
+        tmpbuffer.putLong(rootAddress);
+
+        tmpbuffer.flip();
+        fileChannel.write(tmpbuffer);
+    }
+
+    /**
+     * Reads a {@code BTreeNode} from the disk and returns a {@code BTreeNode} object built from the data
+     * @param diskAddress the address offset, in bytes, for the node in the data file
+     * @return the {@code BTreeNode} object
+     * @throws IOException
+     */
+    public BTreeNode diskRead(long diskAddress) throws IOException {
+        if (diskAddress == 0) {
+            return null;
+        }
+
+        // Set start address to read from
+        fileChannel.position(diskAddress);
+        buffer.clear();
+
+        // Read data into buffer
+        fileChannel.read(buffer);
+        buffer.clear();
+
+        // Read each value from the node
+        int loopMax = this.numNodes; //fixme upper condition might be wrong
+        TreeObject[] keys = new TreeObject[loopMax];
+        for (int i = 0; i < this.nodeSize; i++) {
+            long value = buffer.getLong();
+            long freq = buffer.getLong(); // Read frequency
+
+            String strValue = "" + value; // fixme this is probably wrong
+            keys[i] = new TreeObject(strValue, freq);
+        }
+
+        // Read next byte to get leaf flag
+        byte flag = buffer.get();
+        boolean leaf = (flag == 1);
+
+
+        //TODO finish this based on DiskReadWrite example
+
+        return null; //fixme
+
+    }
+
+    /**
+     * Writes a {@code BTreeNode} to the disk at the specified disk offset address in the {@code BTreeNode} object.
+     * @param node the {@code BTreeNode} to write
+     * @throws IOException
+     */
+    public void diskWrite(BTreeNode node) throws IOException {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public long getSize() {
-        return size;
+        return 0; //TODO
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getDegree() {
-        return degree;
+        return 0; //TODO
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public long getNumberOfNodes() {
-        return nodes;
+        return 0; //TODO
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getHeight() {
-        if (root == null) {
-            return 0;
-        }
-        return 0;
+        return 0; //TODO
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void insert(TreeObject obj) throws IOException {
         /*
@@ -60,8 +198,40 @@ public class BTree implements BTreeInterface {
          */
     }
 
-    private void BtreeSplitChild(BTreeNode rootNode, int index) {
-        BTreeNode y = DiskRead(rootNode.children[index]);
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void dumpToFile(PrintWriter out) throws IOException {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void dumpToDatabase(String dbName, String tableName) throws IOException {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public TreeObject search(String key) throws IOException {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void delete(String key) {
+
+    }
+
+    private void BtreeSplitChild(BTreeNode rootNode, int index) throws IOException {
+        BTreeNode y = diskRead(rootNode.children[index]);
         BTreeNode z = new BTreeNode(new TreeObject[2 * degree - 1], rootNode.leaf()); // new node
         z.leaf = y.leaf;
         z.numKeys = degree - 1;
@@ -80,20 +250,20 @@ public class BTree implements BTreeInterface {
         }
         rootNode.keys[index] = y.keys[degree - 1];
         rootNode.numKeys++;
-        DiskWrite(rootNode);
-        DiskWrite(y);
-        DiskWrite(z);
+        diskWrite(rootNode);
+        diskWrite(y);
+        diskWrite(z);
     }
 
-    private BTreeNode BtreeSplitRoot() {
+    private BTreeNode BtreeSplitRoot() throws IOException {
         BTreeNode s = new BTreeNode(new TreeObject[2 * degree - 1], false);
         s.leaf = false;
         s.numKeys = 0;
         s.children[0] = offset;
-        offset = DiskWrite(s);
+        //offset = diskWrite(s);
         BtreeSplitChild(s, 0);
         return s;
-}
+    }
     /*
     BtreeInsertNonfull()
         i = n-1
@@ -117,43 +287,92 @@ public class BTree implements BTreeInterface {
             BTreeInsertNonFull(x.c[i], k)
 
      */
-    @Override
-    public void dumpToFile(PrintWriter out) throws IOException {
-
-    }
-
-    @Override
-    public void dumpToDatabase(String dbName, String tableName) throws IOException {
-
-    }
-
-    @Override
-    public TreeObject search(String key) throws IOException {
-        return null;
-    }
-
-    @Override
-    public void delete(String key) {
-
-    }
 
     private class BTreeNode {
+
         private TreeObject[] keys;
         private int numKeys;
         private boolean leaf;
         private long[] children;
-        //public static final int BYTES = Integer.BYTES + TreeObject.BYTES + 1 + 3 * Long.BYTES;
+        private long address; // Disk offset (bytes)
+
+        /**
+         * Calculate the size of a node as stored on disk (in bytes). We will store boolean
+         * as 1 byte as its nodeSize is not defined in Java
+         */
+        final private int BYTES = Integer.BYTES + ((2 * degree - 1) * TreeObject.BYTES) + ((2 * degree) * Long.BYTES) + 1;
+
+        /**
+         * TODO
+         * @param keys
+         * @param leaf
+         */
         public BTreeNode(TreeObject[] keys, boolean leaf) {
+            //TODO: validate that keys is length (2 * degree - 1)
+
             this.keys = keys;
             this.leaf = leaf;
-            children = null;
-            numKeys = 0;
+            this.numKeys = 0;
+            this.children = new long[2 * degree];
+
+
+            // Set disk address and update pointer
+            address = nextDiskAddress;
+            nextDiskAddress += nodeSize;
+
         }
 
+        /**
+         * Gets whether this node is a leaf.
+         * @return true if this node is a leaf, false otherwise
+         */
         public boolean leaf() {
             return leaf;
         }
+
+        /**
+         * Creates a new {@code BTreeNode} with an empty {@code keys} array and given leaf status.
+         * @param leaf whether the created node is a leaf (has no children)
+         */
+        public BTreeNode(boolean leaf) {
+            this(new TreeObject[2 * degree - 1], true);
+        }
+
+        /**
+         * Checks if this node is full.
+         * @return true if full, false otherwise
+         */
+        public boolean isFull() {
+            return numKeys == 2 * degree - 1;
+        }
+
+        /**
+         * Gets the key at the given index.
+         * @param i the index
+         * @return the key at index {@code i}
+         */
+        public TreeObject getKey(int i) {
+            return keys[i];
+        }
+
+        /**
+         * Sets the key at the given index to {@code key}.
+         * @param i the index
+         * @param key the value to assign to the given key
+         */
+        public void setKey(int i, TreeObject key) {
+            keys[i] = key;
+        }
+
+        /**
+         * Gets the child pointer at the provided index.
+         * @param i the index
+         * @return the child pointer at index {@code i}
+         */
+        public long getChild(int i) {
+            return children[i];
+        }
     }
 
-}
 
+}
